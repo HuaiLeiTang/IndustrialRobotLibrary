5993549e7eb624d64cae49b2cb82aff58e1f1c0d
HongYoungsuk <crazyhys@naver.com>
2016-02-25 00:58:36 +0800

    TimeOptimization modification - main Test

--- rovin/UnitTester/TOPPTest.cpp
+++ rovin/UnitTester/TOPPTest.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+#include <conio.h>
+#include <rovin\TimeOptimization\TOPP.h>
+
+using namespace std;
+using namespace rovin;
+
+int main()
+{	
+	cout << "Test" << endl;
+
+	_getch();
+	return 0;
+}
